08767f971aabd78c4dbb76dbe401c3e8ebd9b08a
vdimov <vdimov@objectsystems.com>
2020-06-24 22:21:21 +0300

    ex1 completed

--- 01.Linear_Regression/machine-learning-ex1/ex1/computeCost.m
+++ 01.Linear_Regression/machine-learning-ex1/ex1/computeCost.m
@@ -8,6 +8,8 @@ m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
 J = 0;
+xByTheta = X*theta;
+J = (1/(2*m))*(X*theta - y)'*(X*theta - y);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
